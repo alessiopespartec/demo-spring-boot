67b780cfe83ebde36795c41080f19f0f11f13867
Alessio Pes <alessio.pes@par-tec.it>
2024-01-05 09:49:18 +0100

    Spring Security basic

---
+++ src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            System.out.println("User not found with email: " +  email);
+            throw new UsernameNotFoundException("User not found with email: " +  email);
+        }
+
+        UserDetails userDetails =
+                org.springframework.security.core.userdetails.User.builder()
+                        .username(user.getEmail())
+                        .password(user.getPassword())
+                        .roles("USER")
+                        .build();
+
+        return userDetails;
+    }
+}
